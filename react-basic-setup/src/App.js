12c184774183e9097898d3f7ad8cee6e6bcbae0c
Amit <amitm@qnap.com>
2022-12-11 00:23:53 +0530

    Basic setup

---
+++ react-basic-setup/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        React Basic Setup
+      </header>
+    </div>
+  );
+}
+
+export default App;
